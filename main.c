e2c50ee5960d94a81e02cf8d37f5ff50ba79e8d9
alanaruth007 <32655088+alanaruth007@users.noreply.github.com>
2017-11-06 11:41:23 -0500

    Create main.c

---
+++ main.c
@@ -0,0 +1,15 @@
+/*
+Daniel Hug dhug@albany.edu: Leader
+Alana Ruth Aruth@albany.edu : Recorder
+Jessica Kanczura jKanczura@albany.edu : Monitor
+*/
+
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include "prototypes.h"
+
+int main(void){
+
+}
+
