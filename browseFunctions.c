ac428dad0f2400ccd37f0d3012a98ad9bd9ad01e
alanaruth007 <32655088+alanaruth007@users.noreply.github.com>
2017-11-06 15:54:15 -0500

    Function definitions (no bodies yet)

--- browseFunctions.c
+++ browseFunctions.c
@@ -4,8 +4,19 @@ Alana Ruth Aruth@albany.edu : Recorder
 Jessica Kanczura jKanczura@albany.edu : Monitor
 */
 
+#include <stdio.h>
+#include <stdlib.h>
+#include <dirent.h>
 
-//Contains the function that browses through the files
+//Function to browse through directories one at a time, checking for subdirectories with recursion
+void browseDirectories(char* directoryName){
+
+}
+
+//Function to browse through a file by separators (any non numerical/alphabetical char)
+void browseFile(char* fileName){
+
+}
 
 
 
