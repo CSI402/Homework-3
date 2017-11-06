62237a5110c4943c01a47633052dccb061ee8dba
alanaruth007 <32655088+alanaruth007@users.noreply.github.com>
2017-11-06 11:45:00 -0500

    Create browseFunctions.c

---
+++ browseFunctions.c
@@ -0,0 +1,11 @@
+/*
+Daniel Hug dhug@albany.edu: Leader
+Alana Ruth Aruth@albany.edu : Recorder
+Jessica Kanczura jKanczura@albany.edu : Monitor
+*/
+
+
+//Contains the function that browses through the files
+
+
+
