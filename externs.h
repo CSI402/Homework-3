891a745ab6902f436446a144633cecea8dd81e51
alanaruth007 <32655088+alanaruth007@users.noreply.github.com>
2017-11-07 01:41:03 -0500

    Create externs.h

---
+++ externs.h
@@ -0,0 +1,9 @@
+/*
+Daniel Hug dhug@albany.edu: Leader
+Alana Ruth Aruth@albany.edu : Recorder
+Jessica Kanczura jKanczura@albany.edu : Monitor
+
+External variables for the functions
+*/
+
+extern pnode_t h;
