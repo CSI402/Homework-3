ff1144de653ab7ddf5dbc059bf2d7c68ea837b82
alanaruth007 <32655088+alanaruth007@users.noreply.github.com>
2017-11-07 13:53:25 -0500

    linked list of linked lists

--- struct_def.h
+++ struct_def.h
@@ -1,13 +1,22 @@
-/*
 Daniel Hug dhug@albany.edu: Leader
 Alana Ruth Aruth@albany.edu : Recorder
 Jessica Kanczura jKanczura@albany.edu : Monitor
+
+Structure definitions
 */
+
+//Structure for the fileNames and their counts (frequency of word in file)
+typedef struct files{
+  char *fileName;
+  int count;
+  struct files *nextFile;
+} files_t;
+
 
 //Structure for the mapping of the word, filename, count (frequency of word in file)
 typedef struct node{
   char *word;
-  char *fileName;
-  int count;
+  files_t *firstFile;
   struct node *next;
-} node_t, *pnode_t;
+} *pnode_t;
+
