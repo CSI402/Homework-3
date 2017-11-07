f9f3ad5a2915de4e95128bdbdf2e26fa937ffa8c
alanaruth007 <32655088+alanaruth007@users.noreply.github.com>
2017-11-07 02:12:21 -0500

    Create constants.h

---
+++ constants.h
@@ -0,0 +1,11 @@
+/*
+Daniel Hug dhug@albany.edu: Leader
+Alana Ruth Aruth@albany.edu : Recorder
+Jessica Kanczura jKanczura@albany.edu : Monitor
+
+Symbolic constants
+*/
+
+#define TRUE 1
+#define FALSE 0
+
