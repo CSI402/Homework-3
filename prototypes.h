9fa83a149af85dcbf6b241232ab274af42e1022c
alanaruth007 <32655088+alanaruth007@users.noreply.github.com>
2017-11-08 12:55:25 -0500

    Probably all good too

--- prototypes.h
+++ prototypes.h
@@ -1,9 +1,13 @@
-
 /*
 Daniel Hug dhug@albany.edu: Leader
 Alana Ruth Aruth@albany.edu : Recorder
 Jessica Kanczura jKanczura@albany.edu : Monitor
+
+Prototypes of browse and index functions
 */
 
 void browseDirectories(char *directoryName);
 void browseFile(char *fileName);
+void indexGenerator(char *fileName);
+void *checkMalloc(size_t numBytes); //Might not need this
+void freeMem(void *p); //Might not need this
