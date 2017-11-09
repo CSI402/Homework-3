f4cd84cdab60ee8155b5de9f2feeaf39d559a530
Jessica Kanczura <jkanczura@albany.edu>
2017-11-09 00:58:21 -0500

    used curr instead of messing with h

--- indexFunctions.c
+++ indexFunctions.c
@@ -17,93 +17,90 @@ Contains function that creates the index
 
 
 
-  void swap (struct node *a, struct node *b){
-    char* wordTemp = (char*) malloc(20*sizeof(char));
-    strcpy(wordTemp,a->word);
-    strcpy(a->word,b->word);
-    strcpy(b->word,wordTemp);
-    free(wordTemp);
-  }
+void swap (struct node *a, struct node *b){
+  char* wordTemp = (char*) malloc(20*sizeof(char));
+  strcpy(wordTemp,a->word);
+  strcpy(a->word,b->word);
+  strcpy(b->word,wordTemp);
+  free(wordTemp);
+}
 
-  void fileSwap (struct files *a, struct files *b){
-    struct files *temp;
-    temp = a;
-    a = b;
-    b = temp;
-    free(temp);
-  }
+void fileSwap (struct files *a, struct files *b){
+  struct files *temp;
+  temp = a;
+  a = b;
+  b = temp;
+  free(temp);
+}
 
 
 //Function to alphabetize the words in all the files, then sort by count per file
-  //Right now, just prints info to the given fileName
-  void indexGenerator(char *fileName){
-    //Declare a FILE pointer variable
-    FILE *fp;
-    //Sets head to head of linked list containing words
-    struct node *h; 
-      //If given file cannot be opened, print error message and stop
-    if((fp = fopen(fileName, "w+"))== NULL){
-      fprintf(stderr, "Error: File %s cannot be opened.\n", fileName);
-      return;
-     }
-    printf("Calling index generator!\n");
+//Right now, just prints info to the given fileName
+void indexGenerator(char *fileName){
+  //Declare a FILE pointer variable
+  FILE *fp;
+  //Sets head to head of linked list containing words
+  struct node *curr = h;
+  //If given file cannot be opened, print error message and stop
+  if((fp = fopen(fileName, "w+"))== NULL){
+    fprintf(stderr, "Error: File %s cannot be opened.\n", fileName);
+    return;
+  }
+  printf("Calling index generator!\n");
 
 
-    //If the list is empty, print so
-    if (h == NULL){
-      fprintf(stderr, "The list is empty.\n");
-      return;
-    }
+  //If the list is empty, print so
+  if (h == NULL){
+    fprintf(stderr, "The list is empty.\n");
+    return;
+  }
 
-    //Sort words alphabetically
-    struct node *i, *j;
-    for(i = h; i->next != NULL; i = i->next){
-      for(j = i->next; j->next != NULL; j = j->next){
-        if(strcmp(i->word, j->word) > 0){
-          swap(i, j);
-        }
+  //Sort words alphabetically
+  struct node *i, *j;
+  for(i = curr; i->next != NULL; i = i->next){
+    for(j = i->next; j->next != NULL; j = j->next){
+      if(strcmp(i->word, j->word) > 0){
+        swap(i,j);
       }
     }
+  }
 
-    //Sort file counts
-    struct files *x, *y;
-    while(h != NULL){
-    for(x = h->firstFile; x->nextFile != NULL; x = x->nextFile){
+  //Sort file counts
+  struct files *x, *y;
+  while(curr != NULL){
+    for(x = curr->firstFile; x->nextFile != NULL; x = x->nextFile){
       for(y = x->nextFile; y->nextFile != NULL; y = y->nextFile){
         if(x->count < y->count){
           fileSwap(x, y->nextFile);
-          
+
         }
       }
     }
-    h = h->next;
+   curr = curr->next;
   }
 
+  curr = h;
 
 
-    struct files currFile;
+  struct files currFile;
 
-    while (h != NULL){
-      printf("h is not null\n");
+  while (curr != NULL){
+    printf("h is not null\n");
 
-      fprintf(fp, "<list>\n");
-      fprintf(fp,"%s\n", h->word);
-      currFile = *h->firstFile;
+    fprintf(fp, "<list>\n");
+    fprintf(fp,"%s\n", h->word);
+    currFile = *curr->firstFile;
 
-      while(currFile.fileName != NULL){
-        fprintf(fp, "%s %d\n", currFile.fileName, currFile.count);
+    while(currFile.fileName != NULL){
+      fprintf(fp, "%s %d\n", (currFile.fileName), (currFile.count));
 
-        currFile = *currFile.nextFile;
-      }
-      fprintf(fp, "</list>\n");
-      printf("\n");
-      h = h->next;
-      }
+      currFile = *currFile.nextFile;
     }
-
-
-
-
+    fprintf(fp, "</list>\n");
+    printf("\n");
+    curr = curr->next;
+  }
+}
 
 
 
