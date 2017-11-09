69a0db40036e9f2cc75b4c11fb68f6c8cb00f636
Dan Hug <dhug@albany.edu>
2017-11-09 00:06:52 -0500

    Basic logic code for indexfunction, still needs to be debugged

--- indexFunctions.c
+++ indexFunctions.c
@@ -15,46 +15,104 @@ Contains function that creates the index
 #include "struct_def.h"
 #include "externs.h"
 
+
+
+  void swap (struct node *a, struct node *b){
+    char* wordTemp = (char*) malloc(20*sizeof(char));
+    strcpy(wordTemp,a->word);
+    strcpy(a->word,b->word);
+    strcpy(b->word,wordTemp);
+    free(wordTemp);
+  }
+
+  void fileSwap (struct files *a, struct files *b){
+    struct files *temp;
+    temp = a;
+    a = b;
+    b = temp;
+    free(temp);
+  }
+
+
 //Function to alphabetize the words in all the files, then sort by count per file
   //Right now, just prints info to the given fileName
   void indexGenerator(char *fileName){
-  printf("Calling index generator!\n");
-
-  //Declare a FILE pointer variable
-  FILE *fp;
-
-  //If given file cannot be opened, print error message and stop
-  if((fp = fopen(fileName, "w"))== NULL){
-    fprintf(stderr, "Error: File %s cannot be opened.\n", fileName);
-    return;
-  }
-
-
-  //If the list is empty, print so
-  if (h == NULL){
-    fprintf(stderr, "The list is empty.\n");
-    return;
-  }
-
-  pfile_t currFile;
+    //Declare a FILE pointer variable
+    FILE *fp;
+    //Sets head to head of linked list containing words
+    struct node *h; 
+      //If given file cannot be opened, print error message and stop
+    if((fp = fopen(fileName, "w+"))== NULL){
+      fprintf(stderr, "Error: File %s cannot be opened.\n", fileName);
+      return;
+     }
+    printf("Calling index generator!\n");
 
 
-  while (h != NULL){
-    printf("h is not null\n");
-
-    fprintf(fp, "<list>\n");
-    fprintf(fp,"%s\n", h->word);
-    currFile = h->firstFile;
-
-    while(currFile != NULL){
-      fprintf(fp, "%s %d\n", currFile->fileName, currFile->count);
-
-      currFile = currFile->nextFile;
+    //If the list is empty, print so
+    if (h == NULL){
+      fprintf(stderr, "The list is empty.\n");
+      return;
     }
-    fprintf(fp, "</list>\n");
-    printf("\n");
-    h = h->next;
+
+    //Sort words alphabetically
+    struct node *i, *j;
+    for(i = h; i->next != NULL; i = i->next){
+      for(j = i->next; j->next != NULL; j = j->next){
+        if(strcmp(i->word, j->word) > 0){
+          swap(i->word, j->word);
+        }
+      }
+    }
+
+    //Sort file counts
+    struct files *x, *y;
+    while(h != NULL){
+    for(x = h->firstFile; x->nextFile != NULL; x = x->nextFile){
+      for(y = x->nextFile; y->nextFile != NULL; y = y->nextFile){
+        if(x->count < y->count){
+          fileSwap(x->firstFile, y->nextFile);
+          
+        }
+      }
+    }
+    h->next;
   }
-}
+
+
+
+    struct files currFile;
+
+    while (h != NULL){
+      printf("h is not null\n");
+
+      fprintf(fp, "<list>\n");
+      fprintf(fp,"%s\n", h->word);
+      currFile = h->firstFile;
+
+      while(currFile != NULL){
+        fprintf(fp, "%s %d\n", currFile->fileName, currFile->count);
+
+        currFile = currFile->nextFile;
+      }
+      fprintf(fp, "</list>\n");
+      printf("\n");
+      h = h->next;
+      }
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
--- prototypes.h
+++ prototypes.h
@@ -11,3 +11,5 @@ void browseFile(char *fileName);
 void indexGenerator(char *fileName);
 void *checkMalloc(size_t numBytes); //Might not need this
 void freeMem(void *p); //Might not need this
+void swap(struct node *a, struct node *b);
+void fileSwap(struct files *a, struct files *b);
